a02f3728507deec12a24feddfb6184fb4fe1d5ac
Devi Prasad <prasadpjd@gmail.com>
2024-08-06 21:45:34 +0530

    Reusable Banners App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import BannerCardItem from './components/BannerCardItem'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const bannerCardsList = [
+  {
+    uniqueId:1,
+    headerText: "Our New Designs",
+    description: "Get the designs developed by our in-house team all for yourself",
+    className: "card-2",
+  },
+  {
+    uniqueId:2,
+    headerText: "Insiders",
+    description: "Get the top class products for yourself with an extra off",
+    className: "card-3",
+  },
+  {
+    uniqueId:3,
+    headerText: "The Seasons Latest",
+    description: "Get the seasons all latest designs in a flick of your hand",
+    className: "card-1",
+  },
+]
+const App = () => (
+  <div className='app-container'>
+    <ul className='banners-card-list'>
+      {bannerCardsList.map(eachItem => (
+        <BannerCardItem bannerDetails = {eachItem} key={eachItem.uniqueId}/>
+      ))}
+    </ul>
+  </div>
+)
 export default App;
